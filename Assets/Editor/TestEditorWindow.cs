6fa0d7d6662f0104ec646a854c822a675e20bb02
kylekim95 <kylekim95@gmail.com>
2021-04-03 21:00:20 +0900

    04/03
    음악 에디터 패널
    1. 음악 탐색 및 불러오기 구현
    2. 불러온 음악을Play, Pause, Stop 할 수 있는 기능 구현

--- Assets/Editor/TestEditorWindow.cs
+++ Assets/Editor/TestEditorWindow.cs
@@ -1,74 +1,80 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.Windows;
 
 public class TestEditorWindow : EditorWindow
 {
-    Texture2D headerSectionTexture;
-    Color headerSectionColor = new Color(13f/255, 32f/255, 44f/255, 1f);
-    Rect headerSection;
-    
-    Texture2D ImgButtonTexture;
-
+    AudioClip audioClip;
+    GameObject audioSourceObj;
+    AudioSource audioSource;
+    enum audioClipState{
+        play, pause, stop
+    }
     [MenuItem("Window/TestEditorWindow")]
-    static void OpenWindow()
+    public static void ShowExample()
     {
-        TestEditorWindow tes = (TestEditorWindow)GetWindow(typeof(TestEditorWindow));
-        //restrain size of the window
-        tes.minSize = new Vector2(600, 300);
-        tes.Show();
+        TestEditorWindow wnd = GetWindow<TestEditorWindow>();
+        wnd.titleContent = new GUIContent("TestEditorWindow");
     }
-    void OnEnable()
+    public void CreateGUI()
     {
-        InitTextures();
-    }
-    void InitTextures()
-    {
-        headerSectionTexture = new Texture2D(1, 1);
-        headerSectionTexture.SetPixel(0, 0, headerSectionColor);
-        headerSectionTexture.Apply();
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
 
-        ImgButtonTexture = new Texture2D(25, 25);
-        for(int i = 0; i < 25; i++)
-        {
-            for(int j = 0; j < 25; j++)
+        // Import UXML
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/TestEditorWindow.uxml");
+        VisualElement labelFromUXML = visualTree.Instantiate();
+        root.Add(labelFromUXML);
+
+        //Buttons
+        Button openExplorerBtn = rootVisualElement.Query<Button>("OpenExplorerBtn");
+        openExplorerBtn.clickable.clicked += OpenExplorer;
+
+        Button playBtn = rootVisualElement.Query<Button>("Play");
+        Button pauseBtn = rootVisualElement.Query<Button>("Pause");
+        Button stopBtn = rootVisualElement.Query<Button>("Stop");
+        playBtn.clickable.clicked += ()=>AudioControl(audioClipState.play);
+        pauseBtn.clickable.clicked += ()=>AudioControl(audioClipState.pause);
+        stopBtn.clickable.clicked += ()=>AudioControl(audioClipState.stop);
+    }
+    private void OnDestroy() {
+        audioSource.Stop();
+        GameObject.DestroyImmediate(audioSourceObj);
+    }
+    void OpenExplorer(){
+        string path = EditorUtility.OpenFilePanel("Audio file explorer",
+            "C:/Users/kylek/Desktop/Code/Unity/Projects/KusoGame/5/KusoRhythmGame/Assets/Resources/Audio",
+            "mp3,mp4"
+        );
+        if(path.Length != 0){
+            audioClip = NAudioPlayer.FromMp3Data(File.ReadAllBytes(path));
+            if(audioSourceObj == null)
             {
-                ImgButtonTexture.SetPixel(i,j,Color.red);
+                audioSourceObj = new GameObject("EditorAudioSource");
+                audioSourceObj.hideFlags |= HideFlags.HideInHierarchy;
+                audioSource = audioSourceObj.AddComponent<AudioSource>();
+                audioSource.clip = audioClip;
             }
         }
-        ImgButtonTexture.Apply();
     }
-    void OnGUI()
-    {
-        DrawLayouts();
-        DrawHeader();
-        //mouse up 이벤트가 일정 영역에서 발생시 실행
-        if(Event.current.type == EventType.MouseUp)
-        {
-            Vector2 mousePos = Event.current.mousePosition;
-            if (mousePos.x >= headerSection.x && mousePos.x <= headerSection.width - headerSection.x &&
-                mousePos.y >= headerSection.y && mousePos.y <= headerSection.height - headerSection.y)
-            {
-                AudioClip ac = (AudioClip)Resources.Load("");
-            }
+    void AudioControl(audioClipState state){
+        switch((int)state){
+            case 0:
+                Debug.Log("Play");
+                if(audioSource.isPlaying)
+                    audioSource.UnPause();
+                else
+                    audioSource.Play();
+            break;
+            case 1:
+                Debug.Log("Pause");
+                audioSource.Pause();
+            break;
+            case 2:
+                Debug.Log("Stop");
+                audioSource.Stop();
+            break;            
         }
-    }
-    void DrawLayouts()
-    {
-        headerSection.x = 0;
-        headerSection.y = 0;
-        headerSection.width = Screen.width;
-        headerSection.height = 50;
-
-        GUI.DrawTexture(headerSection, headerSectionTexture);
-    }
-    void DrawHeader()
-    {
-        GUILayout.BeginArea(headerSection);
-        GUILayout.BeginHorizontal();
-
-        GUILayout.EndHorizontal();
-        GUILayout.EndArea();
     }
 }
